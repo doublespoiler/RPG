1be7b69c8ff3ccbaed0c2567825cae81c2eac7e2
Harold_Mesa <harold.mesa@icloud.com>
2022-08-31 13:46:57 -0500

    Pass 3rd TDD
    Co-authored-by: Skylan Lew <skylanslew@gmail.com>
    Co-authored-by: Ted Gold <ted.jonathan.gold@gmail.com>

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -16,4 +16,9 @@ describe('Character', () => {
   test('should add stats to character object', () => {
     expect(character.statsArray).toEqual([1, 2, 3, 4, 5]);
   });
+
+  test('should set current HP to MaxHP (statsArray[4])', () => {
+    character.setHp(character.statsArray[4]);
+    expect(character.currentHp).toEqual(character.statsArray[4]);
+  });
 });
--- src/character.js
+++ src/character.js
@@ -3,12 +3,16 @@ export default class Character {
     this.name = name;
     this.type = type;
     this.statsArray = []; //this includes stats that will change when we level up: attack, defense, speed, accuracy, maxHp
-    //track current hp and xp through constructor
+    this.currentHp;
+    //track current xp through constructor
   }
   setStats(array) {
     array.forEach(element => {
       this.statsArray.push(element);
     });
+  }
+  setHp(setHpTo) {
+    this.currentHp = setHpTo;
   }
 }
 
